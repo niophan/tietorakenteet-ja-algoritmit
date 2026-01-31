c2458d874510293d1474c147bbac166f7ae2de1a
Nhan Phan <nio.phan@tuni.fi>
2026-02-01 00:43:31 +0200

    palauta ratkaisu_kierros_3_tehtävä_1b

--- kierros3/main.cpp
+++ kierros3/main.cpp
@@ -8,20 +8,20 @@ Post: The numbers are printed in reverse order.
 Uses: The STL class stack and its methods */
 {
   int n;
-  double item;
-  stack<double> numbers; // declares and initializes a stack of numbers
+  char item;
+  stack<char> characters; // declares and initializes a stack of numbers
   cout << " Type in an integer n followed by n decimal numbers." << endl
         << " The numbers will be printed in reverse order." << endl;
   cin >> n;
 
   for (int i = 0; i < n; i++) {
     cin >> item;
-    numbers.push(item);
+    characters.push(item);
   }
   cout << endl << endl;
-  while (!numbers.empty( )) {
-    cout << numbers.top( ) << " ";
-    numbers.pop( );
+  while (!characters.empty( )) {
+    cout << characters.top( ) << " ";
+    characters.pop( );
   }
   cout << endl;
 }
