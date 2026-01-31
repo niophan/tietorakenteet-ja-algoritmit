7787a37565918e546fbcfdbfa498f4a897616319
Nhan Phan <nio.phan@tuni.fi>
2026-02-01 00:51:42 +0200

    palauta ratkaisu_kierros_3_tehtävä_1c

--- kierros3/main.cpp
+++ kierros3/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <stack>
+#include <string>
 using namespace std;
+
+bool is_palindrome(const string& text) {
+  stack<char>pa;
+  for (char c : text) pa.push(c);
+  for (char c : text) {
+    if (c != pa.top()) return false;
+    pa.pop();
+  }
+  return true;
+}
+
 
 int main( )
 /* Pre: The user supplies an integer n and n decimal numbers.
@@ -9,14 +21,22 @@ Uses: The STL class stack and its methods */
 {
   int n;
   char item;
+  string m;
   stack<char> characters; // declares and initializes a stack of numbers
   cout << " Type in an integer n followed by n decimal numbers." << endl
-        << " The numbers will be printed in reverse order." << endl;
+        << " The text will be printed in reverse order." << endl;
   cin >> n;
 
   for (int i = 0; i < n; i++) {
     cin >> item;
     characters.push(item);
+    m += item;
+  }
+
+  if(is_palindrome(m)) {
+    cout << "Given string is a palindrome." << endl;
+  } else {
+    cout << "Given string is a normal text." << endl;
   }
   cout << endl << endl;
   while (!characters.empty( )) {
