1b380ffbb8711b2f71396f1ad10a03ee22e10e0b
Nhan Phan <nio.phan@tuni.fi>
2026-03-14 23:57:15 +0200

    palauta ratkaisu_kierros8_tehtävä_1

---
+++ kierros8/LinkedQueue.h
@@ -0,0 +1,81 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+enum Error_code { success, overflow, underflow };
+typedef double Queue_entry;
+typedef Queue_entry Node_entry;
+
+class Queue {
+  public:
+    // standard Queue methods
+    Queue() { front = rear = nullptr; }
+    bool empty() const {
+      return front == nullptr;
+    }
+
+    Error_code append(const Queue_entry &item)
+    {
+      Node *new_rear = new Node(item);
+      if (new_rear == nullptr) return overflow;
+      if (rear == nullptr) front = rear = new_rear;
+      else {
+        rear->next = new_rear;
+        rear = new_rear;
+      }
+      return success;
+    }
+
+
+    Error_code serve() {
+      if (front == nullptr) return underflow;
+      Node *old_front = front;
+      front = old_front->next;
+      if (front == nullptr) rear = nullptr;
+      delete old_front;
+      return success;
+    }
+
+
+    Error_code retrieve(Queue_entry &item) const {
+      if (front == nullptr) return underflow;
+      item = front->entry;
+      return success;
+    }
+
+    // safety features for linked structures
+    ~Queue() {
+      while(!empty())
+        serve();
+    }
+
+    Queue(const Queue &original) {
+      front = rear = nullptr;
+      Node *temp = original.front;
+      while (temp != nullptr) {
+          append(temp->entry);
+          temp = temp->next;
+      }
+    }
+
+    void operator = (const Queue &original) {
+      if(this == &original) return;
+      while(!empty()) serve();
+      Node *temp = original.front;
+      while(temp!= nullptr) {
+        append(temp->entry);
+        temp = temp->next;
+      }
+    }
+  
+  protected:
+   struct Node {
+      Node_entry entry;
+      Node *next;
+
+      Node() : entry(0), next(nullptr) {}
+      Node(Node_entry item, Node *add_on = nullptr) : entry(item), next(add_on) {}
+    };
+    Node *front, *rear;
+};
+
+#endif
