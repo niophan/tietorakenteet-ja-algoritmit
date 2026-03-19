a852ec199c70c76e75732a35111e186fd7d3d9fc
Nhan Phan <nio.phan@tuni.fi>
2026-03-19 18:15:45 +0200

    palauta ratkaisu_kierros9_tehtävä_3

--- kierros9/main.cpp
+++ kierros9/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 
@@ -28,6 +29,19 @@ int f2(int n) {
   return -1;
 }
 
+void move(int count, int start, int finish, int temp,
+          float& transfer_orders,
+          float& move_calls) {
+  move_calls += 1;
+
+  if (count > 0) {
+    move(count - 1, start, temp, finish, transfer_orders, move_calls);
+    // cout << "Move disk "  << count << " from" << start << " to " << finish << "." << endl;
+    transfer_orders += 1;
+    move(count - 1, temp, finish, start, transfer_orders, move_calls);
+  }
+}
+
 
 
 int main() {
@@ -48,6 +62,30 @@ int main() {
   cout << f2(5) << endl;
   cout << f2(6) << endl;
 
+  cout << "Tehtävä 3" << endl;
+  double previous_time_ms = 0.0;
+  for (int disks = 10; disks <= 15; ++disks) {
+      float transfer_orders = 0; // show algorithmic work of Hanoi operations
+      float move_calls = 0; // show recursion overhead
+
+    auto start = chrono::high_resolution_clock::now();
+      move(disks, 1, 3, 2, transfer_orders, move_calls);
+    auto end = chrono::high_resolution_clock::now();
+
+    double elapsed_ms = chrono::duration<double, milli>(end - start).count();
+
+    cout << "disks=" << disks
+        << " transfer_orders=" << transfer_orders
+        << " move_calls=" << move_calls
+         << " time_ms=" << elapsed_ms;
+
+    if (previous_time_ms > 0.0) {
+      cout << " time_ratio=" << (elapsed_ms / previous_time_ms);
+    }
+
+    cout << endl;
+    previous_time_ms = elapsed_ms;
+  }
 
   return 0;
 }
